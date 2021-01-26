9baa012f9a4eaaec9a1e98162032ae2f522895ed
sjai0003 <shubham.jaiswal@synchronoss.com>
2021-01-27 03:12:15 +0530

    Initial Commits

---
+++ src/main/java/com/example/practice/sfgdi/SfgDiApplication.java
@@ -0,0 +1,12 @@
+package com.example.practice.sfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SfgDiApplication.class, args);
+  }
+}
